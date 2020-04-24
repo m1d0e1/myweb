0ee687fda040c848a911f3bd3b8c605d4077d38e
m1d0e1 <32526873+m1d0e1@users.noreply.github.com>
2020-04-24 22:25:20 +0200

    Add files via upload

---
+++ js/plugin.js
@@ -0,0 +1,40 @@
+$(".option").click(function(){
+    $(".color").fadeToggle();
+});
+
+$('.carousel').carousel({
+    interval: 2000
+  })
+
+$(function() {  
+    $("html").niceScroll();
+});
+
+newFunction();
+function newFunction() {
+    var color=$(".color ul li");
+    color
+        .eq(0).css("backgroundColor", "#E41B17").end()
+        .eq(1).css("backgroundColor", "#5a169e").end()
+        .eq(2).css("backgroundColor", "#008639").end()
+        .eq(3).css("backgroundColor", "#961c95").end()
+        .eq(4).css("backgroundColor", "#044680");
+    ;
+}
+//color.click(function(){
+//    $("link[href*='theme']").attr("href".$(this).attr("data-value"));
+//});
+$('.color ul li').on('click', function(){
+    var path = $(this).data('path');
+    $('#color-switcher').attr('href', path);
+});
+$( window ).ready(function() {
+    $(".load .sk-chase").fadeOut(2000,
+        function(){
+            $(this).parent().fadeOut(2000,
+                function(){
+                    $("body").css("overflow","auto")
+                    $(this).remove();
+                });
+        });
+  });
